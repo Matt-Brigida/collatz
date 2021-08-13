69966bbd4f5bceb7368081440d1d8d0df97eff30
Matt Brigida <mdbrigida@gmail.com>
2021-08-13 15:45:12 -0400

    initializing big_number, still segfault

--- collatz.c
+++ collatz.c
@@ -6,13 +6,19 @@ void collatz(mpz_t big_number, mpz_t return_value);
 int
 main(void)
 {
+
+  int counter = 0;
  
   const char* big_num = "454353534483274829347329847298347928374982347293847293847239429342934299247";
+
+  mpz_t big_number;
+  mpz_init(big_number);
+  mpz_set_str(big_number, big_num, 10);
 
   mpz_t return_value;
   mpz_init(return_value);
 
-  collatz(big_num, return_value);
+  collatz(big_number, return_value);
 
   void (*null_ptr)() = NULL;
   // int* null_ptr=NULL;
@@ -29,11 +35,11 @@ void collatz(mpz_t big_number, mpz_t return_value)
   
   if (mpz_even_p(big_number))
     {
-      mpz_cdiv_q(return_value, big_number, two);
+      mpz_cdiv_q(&return_value, big_number, two);
     }
   else
     {
-      mpz_mul_ui(return_value, big_number, 3);
-      mpz_add_ui(return_value, return_value, 1);
+      mpz_mul_ui(&return_value, big_number, 3);
+      mpz_add_ui(&return_value, return_value, 1);
     }
 }
