149238710337380960c034ae020c23844e2e5793
Matt Brigida <mdbrigida@gmail.com>
2021-08-14 14:07:24 -0400

    works!!!!!!!

--- collatz.c
+++ collatz.c
@@ -7,7 +7,7 @@ int
 main(void)
 {
     
-  const char* big_num = "454353534483274829347329847298347928374982347293847293847239429342934299247";
+  const char* big_num = "4543535344832748293473298472983479283749823472938472938472394293429342992478994561233456531529582812592587348452298844153600399";
 
   mpz_t big_number;
   mpz_init(big_number);
@@ -31,17 +31,25 @@ void collatz(mpz_t big_number)
       counter = counter + 1;
    if (mpz_even_p(big_number))
     {
+      /* void (*null_ptr)() = NULL; */
+      /* mpz_out_str(null_ptr,10, big_number); */
+      gmp_printf ("Collatz number %Zd\n", big_number);
+
       mpz_cdiv_q(big_number, big_number, two);
       collatz(big_number);
     }
    else 
     {
+      /* void (*null_ptr)() = NULL; */
+      /* mpz_out_str(null_ptr,10, big_number); */
+      gmp_printf ("Collatz number %Zd\n", big_number);
+
       mpz_mul_ui(big_number, big_number, 3);
       mpz_add_ui(big_number, big_number, 1);
       collatz(big_number);
     }
     }
-    printf("The counter is: %d\n", counter);
+    /* printf("The counter is: %d\n", counter); */
 }
     
 
