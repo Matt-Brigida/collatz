841d876caa8ade25e293ada64aa193570c221d34
Matt Brigida <mdbrigida@gmail.com>
2021-08-14 13:54:53 -0400

    this should work...

--- collatz.c
+++ collatz.c
@@ -1,45 +1,52 @@
 #include <stdio.h>
 #include <gmp.h>
 
-void collatz(mpz_t big_number, mpz_t return_value);
+void collatz(mpz_t big_number);
 
 int
 main(void)
 {
-
-  int counter = 0;
- 
+    
   const char* big_num = "454353534483274829347329847298347928374982347293847293847239429342934299247";
 
   mpz_t big_number;
   mpz_init(big_number);
   mpz_set_str(big_number, big_num, 10);
 
-  mpz_t return_value;
-  mpz_init(return_value);
-
-  collatz(big_number, return_value);
-
-  void (*null_ptr)() = NULL;
-  // int* null_ptr=NULL;
-  mpz_out_str(null_ptr,10, return_value);
-  return 0;
+  collatz(big_number);
+  
 }
 
-void collatz(mpz_t big_number, mpz_t return_value)
+void collatz(mpz_t big_number)
 {
+
+  int counter = 0;
 
   mpz_t two;
   mpz_init(two);
   mpz_set_si(two, 2);
-  
-  if (mpz_even_p(big_number))
+
+  while(!(mpz_cmp_d(big_number, 1) == 0))
     {
-      mpz_cdiv_q(return_value, big_number, two);
-    }
-  else
+      counter = counter + 1;
+   if (mpz_even_p(big_number))
     {
-      mpz_mul_ui(return_value, big_number, 3);
-      mpz_add_ui(return_value, return_value, 1);
+      mpz_cdiv_q(big_number, big_number, two);
+      collatz(big_number);
     }
+   else 
+    {
+      mpz_mul_ui(big_number, big_number, 3);
+      mpz_add_ui(big_number, big_number, 1);
+      collatz(big_number);
+    }
+    }
+    printf("The counter is: %d\n", counter);
 }
+    
+
+
+/*** way to print big number
+  void (*null_ptr)() = NULL;
+  mpz_out_str(null_ptr,10, counter);
+***/
